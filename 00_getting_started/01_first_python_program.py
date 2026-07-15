6d4f5c1de09d55ad68cd2e34199fcf525d6df598
deep <dipvadukiya06@gmail.com>
2026-07-15 17:29:48 +0200

    PY01: first python program

---
+++ 00_getting_started/01_first_python_program.py
@@ -0,0 +1,19 @@
+# all the python code is written in the .py file
+
+# To run the code, you can use the terminal or command prompt. Navigate to the directory where your .py file is located and type `python3 filename.py` (replace `filename.py` with the name of your Python file). This will execute the code in the file and display any output in the terminal.
+
+print("Hello, World!")  # This line prints "Hello, World!" to the console
+
+print("*" * 20)  # This line prints a line of asterisks for visual separation
+
+
+# you can install autopep8 using pip. Open your terminal or command prompt and type the following command:
+# pip install autopep8
+
+
+# you can bind the key to run the python code in the .py file. You can use the following keybinding in VS Code:
+# {
+#     "key": "ctrl+r",
+#     "command": "python.execInTerminal",
+#     "when": "editorTextFocus && editorLangId == 'python'"
+# }
