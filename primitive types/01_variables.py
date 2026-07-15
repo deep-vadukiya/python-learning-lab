67f9156cb7f9288377cb13eefba6e0c3766f82c0
deep <dipvadukiya06@gmail.com>
2026-07-15 17:35:07 +0200

    PY01: variables

---
+++ primitive types/01_variables.py
@@ -0,0 +1,13 @@
+##
+
+students_count = 1000
+
+print(students_count)
+
+# primitive types can be: numbers, strings, booleans, and NoneType
+
+rating = 4.9  # float
+is_published = True  # boolean
+course_name = "Python Programming"  # string
+
+# it is recommended to use the appropriate variable names that are descriptive and meaningful. For example, instead of using a variable name like "x" or "y", use a name like "students_count" or "course_name". This makes the code more readable and easier to understand.
