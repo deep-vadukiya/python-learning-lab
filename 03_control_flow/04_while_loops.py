06ff975dbbf5b1a7d207aaac29877f1c97f9043b
deep <dipvadukiya06@gmail.com>
2026-07-20 18:30:09 +0200

    PY03: while loop

---
+++ 03_control_flow/04_while_loops.py
@@ -0,0 +1,13 @@
+# while loops ...
+
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+
+command = ""
+# while command.lower() != "quit":
+#     command = input(">")
+#     print("ECHO", command)
+
+# infinite loops ...
